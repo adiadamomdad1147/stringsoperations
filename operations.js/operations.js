51d1d371f0a30ca2667e58db1cd0a06c6ab5d197
adiadamomdad1147 <130950599+adiadamomdad1147@users.noreply.github.com>
2023-11-15 17:19:45 +0000

    strings

---
+++ operations.js/operations.js
@@ -0,0 +1,41 @@
+
+//********************************** OPERATIONS *********************************
+
+//[1] ********************************** NEGATIVE VALUE *********************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+//[2]********************************** MULTIPLICATION *********************************
+
+let value1 = 5
+let value2 = 4
+let multiValue = value1*value2 
+console.log(multiValue);
+
+//[3]********************************** DIVISION *********************************
+
+let value3 = 5
+let value4 = 4
+let divValue = value3/value4 
+console.log(divValue);
+[4]********************************** MODULUS *********************************
+
+let value5 = 5
+let value6 = 4
+let modulusValue = value5%value6 
+console.log(modulusValue);
+
+// [5]****************************** ADDITION OF STRINGS *********************************
+
+let str1 = "ADITYA"
+let str2 = "KUMAR"
+let str3 = str1 + str2 
+console.log(str3);
+
+//############################### PREFIX & POSTFIX ########################################################
+
+let gameCounter = 7000
+++gameCounter
+console.log(gameCounter);
